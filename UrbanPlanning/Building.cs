64ddfd71b73e2850a276267f75c477a191a59ed5
Jordan Smith <jordansmith989@gmail.com>
2021-01-23 10:40:43 -0600

    urban plans

---
+++ UrbanPlanning/Building.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanning
+{
+    class Building
+    {
+
+        // you will need this collection at some point List<int> myListOfIntegers = new List<int>();
+
+        //Fields
+        private string _designer = "Jordan Smith";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        //Properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        //Volume property read only and returns math
+        public double Volume { get { return Width * Depth * (3 * Stories); } }
+
+        //Constructor
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        //Method
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+        public void Purchase(string name)
+        {
+            _owner = name;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($@"
+                {_address}
+                Designed by {_designer}
+                Date Constructed: {_dateConstructed}
+                Owner: {_owner}
+                {Volume} cubic meters of space
+            ");
+        }
+    }
+}
---
+++ UrbanPlanning/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UrbanPlanning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEight = new Building("512 8th Avenue")
+            {
+                Width = 30.7,
+                Depth = 60.3,
+                Stories = 4,
+            };
+            Building SearsTower = new Building("233 S. Wacker Dr.")
+            {
+                Width = 225,
+                Depth = 100.5,
+                Stories = 108,
+            };
+            Building FarmHouse = new Building("1314 W. Cullerton St.")
+            {
+                Width = 40,
+                Depth = 40,
+                Stories = 1,
+            };
+
+            FarmHouse.Construct();
+            SearsTower.Construct();
+            FiveOneTwoEight.Construct();
+
+            FarmHouse.Purchase("Michael Jordan");
+            SearsTower.Purchase("Holly Smith");
+            FiveOneTwoEight.Purchase("Gordon Ramsay");
+
+            FarmHouse.ShowInfo();
+            SearsTower.ShowInfo();
+            FiveOneTwoEight.ShowInfo();
+        }
+    }
+}
